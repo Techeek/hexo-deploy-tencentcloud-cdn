fa8c2666f779fa4f3db132495537c6907ad9dd62
Techeek <cheng@techeek.cn>
2018-10-29 15:02:11 +0800

    init

---
+++ index.js
@@ -0,0 +1,1 @@
+hexo.extend.deployer.register('hexo-deploy-TencentCloud-CDN', require('./libs/push'));
---
+++ libs/push.js
@@ -0,0 +1,15 @@
+var qcloudSDK = require('./submit'); 
+var config = this.config;
+var secret_Id = config.tencentcdn.secretId;
+var secret_Key = config.tencentcdn.secretKey;
+var url = config.url
+qcloudSDK.config({
+    secretId: secret_Id,
+    secretKey: secret_Key
+})
+
+qcloudSDK.request('RefreshCdnUrl', {
+    'urls.1': url
+}, (res) => {
+console.log('腾讯云CDN首页刷新推送结果' + res);
+})
